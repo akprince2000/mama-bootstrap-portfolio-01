0fef5a8d8d750c652f011a65e5f42f3d079886ce
prince954 <akprince0268@gmail.com>
2022-03-12 03:08:04 -0800

    skills-bar

---
+++ js/plugin/active.js
@@ -0,0 +1,51 @@
+(function($) {
+    'use strict';
+
+    $('.web_design').rProgressbar({
+        percentage: 80,
+        fillBackgroundColor: '#30BBD1',
+    });
+        
+
+
+    $('.angular_js').rProgressbar({
+        percentage: 80,
+        fillBackgroundColor: '#30BBD1'
+    });
+
+
+
+    $('.html_css').rProgressbar({
+        percentage: 95,
+        fillBackgroundColor: '#DC7A48'
+    });
+
+
+
+    $('.bootstrap').rProgressbar({
+        percentage: 95,
+        fillBackgroundColor: '#DC7A48'
+    })
+
+    $('.java_script').rProgressbar({
+        percentage: 60,
+        fillBackgroundColor: '#B74BCC'
+    });
+
+    $('.graphic_design').rProgressbar({
+        percentage: 60,
+        fillBackgroundColor: '#B74BCC',
+    });
+
+
+    $('.react_js').rProgressbar({
+        percentage: 85,
+        fillBackgroundColor: '#E66770',
+    });
+
+    $('.ui_ux_dedign').rProgressbar({
+        percentage: 85,
+        fillBackgroundColor: '#E66770',
+    });
+
+})(jQuery);
