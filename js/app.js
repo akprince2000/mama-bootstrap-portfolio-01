3bc9096a233ccaece7ab2ee04016921e9f338cf4
prince954 <akprince0268@gmail.com>
2022-03-14 04:07:50 -0700

    slider

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+// slider Start
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:false,
+    nav:true,
+    dots:false,
+});
+
